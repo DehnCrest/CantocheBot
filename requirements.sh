16f4d782f31865160b2e8aa096a4df83d4bd893c
DehnCrest <dehncrest.yt@gmail.com>
2021-11-20 16:35:08 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,4 @@
-pip3 install discord
-pip3 install pdf2image
-pip3 install pdfplumber
+python3 -m pip install discord
+python3 -m pip install pillow==8.0.0
+python3 -m pip install pdf2image
+python3 -m pip install pdfplumber
